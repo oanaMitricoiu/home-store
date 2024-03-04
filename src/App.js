b30b1c860d1b896f9aea42abafec8ed216486828
oana-iu <oana.mitri01@gmail.com>
2024-03-04 15:33:33 +0200

    update products reducer

--- src/App.js
+++ src/App.js
@@ -2,44 +2,43 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 import {
-  Home,
-  Products,
-  SingleProduct,
-  About,
-  Cart,
-  Error,
-  Checkout,
-  PrivateRoute,
-  AuthWrapper,
+    Home,
+    Products,
+    SingleProduct,
+    About,
+    Cart,
+    Error,
+    Checkout,
+    PrivateRoute,
+    AuthWrapper,
 } from "./pages";
-import { BsRouter } from "react-icons/bs";
 
 function App() {
-  return (
-    <AuthWrapper>
-      <Router>
-        <Navbar />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:id" element={<SingleProduct />} />
-          <Route
-            path="checkout"
-            element={
-              <PrivateRoute>
-                <Checkout />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthWrapper>
-  );
+    return (
+        <AuthWrapper>
+            <Router>
+                <Navbar />
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="cart" element={<Cart />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="products/:id" element={<SingleProduct />} />
+                    <Route
+                        path="checkout"
+                        element={
+                            <PrivateRoute>
+                                <Checkout />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </AuthWrapper>
+    );
 }
 
 export default App;
